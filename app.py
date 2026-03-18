f51086fc56000b2eea5f3adfe0f8e4362d199190
Zachary Wilson <8090008+z-wilson@users.noreply.github.com>
2026-03-18 15:01:54 -0500

    feat: initial project upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+VERSION = "1.0.0"
+
+@app.route('/')
+def home():
+    return {"status": "ok", "version": VERSION}
+
+@app.route('/time')
+def time():
+    return {"time": datetime.now().isoformat(), "env": os.getenv("ENV", "dev-env")}
+
+if __name__ == '__main__':
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
